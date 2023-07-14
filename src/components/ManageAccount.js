ad25aa56366754e7a8c39e407864a5f7c8b5ee4e
jj <jj@gmail.com>
2023-07-14 12:50:54 -0700

    changes

--- src/components/ManageAccount.js
+++ src/components/ManageAccount.js
@@ -1,23 +1,118 @@
-import { Button, Panel, DropdownButton, MenuItem, FormControl } from 'react-bootstrap';
+import { Button, Panel, DropdownButton, MenuItem, FormControl, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import createReactClass from 'create-react-class';
 
+
+const TooltipWrapper = ({ message, children }) => {
+  return <OverlayTrigger placement={'top'} overlay={<Tooltip>
+    <strong>{message}</strong>
+  </Tooltip>}>
+    {children}
+  </OverlayTrigger>
+}
+
+var options = [
+  // 添加你需要的选项
+  { label: "选项1", value: "Option1" },
+  { label: "选项2", value: "Option2" },
+  { label: "选项3", value: "Option11" },
+  { label: "选项24", value: "Option21" },
+  { label: "选项14", value: "Option111" },
+  { label: "选项25", value: "Option22" },
+  { label: "选项16", value: "Option121" },
+  { label: "选项27", value: "Option22" },
+  { label: "选项18", value: "Option14" },
+  { label: "选项29", value: "Option23" },
+  { label: "选项10", value: "Option15" },
+  { label: "选项29", value: "Option27" },
+  // ...
+];
+
+
 const ManageAccount = createReactClass({
-  getInitialState: function() {
+  getInitialState: function () {
     return {
-      accountTypes: ['type1', 'type2', 'type3'],
+      accountTypes: ['iam_arn', 'basic_auth_user'],
       accountNames: ['name1', 'name2', 'name3'],
       odinMaterialSet: '',
-      posixGroups: ['group1', 'group2', 'group3']
+      posixGroups: ['group1', 'group2', 'group3'],
+      regions: ['USA', 'AUS', 'CHINA'],
+      selected: [],
+      hover: null,
+      accountType: '',
+      accountName: '',
+      posixGroup: '',
+      region: ''
     };
   },
 
-  handleOdinMaterialSetChange: function(e) {
+  handleSelect: function (value, event) {
+    // 阻止事件冒泡，防止下拉菜单关闭
+    event.stopPropagation();
+
+    this.setState(function (prevState) {
+      if (prevState.selected.includes(value)) {
+        // 如果已选中，则取消选中
+        return { selected: prevState.selected.filter(function (item) { return item !== value; }) };
+      } else {
+        // 如果未选中，则添加到选中列表
+        return { selected: prevState.selected.concat(value) };
+      }
+    });
+  },
+
+  handleRemove: function (value) {
+    this.setState(function (prevState) {
+      return { selected: prevState.selected.filter(function (item) { return item !== value; }) };
+    });
+  },
+
+
+  handleMouseEnter: function (value) {
+    this.setState({ hover: value });
+  },
+
+  handleMouseLeave: function () {
+    this.setState({ hover: null });
+  },
+
+
+  handleOdinMaterialSetChange: function (e) {
     this.setState({
       odinMaterialSet: e.target.value
     });
   },
 
-  render: function() {
+  handleClickAccountType: function (type) {
+
+    this.setState({
+      accountType: type
+    })
+  },
+
+  handleClickAccountName: function (name) {
+    this.setState({
+      accountName: name
+    })
+  },
+
+  handleClickPosixGroup: function (group) {
+    this.setState({
+      posixGroup: group
+    })
+  },
+
+  handleClickRegion: function (region) {
+    this.setState({
+      region: region
+    })
+  },
+
+  handleSubmit: function (e) {
+    // submit create
+    console.log(this.state)
+  },
+
+  render: function () {
     return (
       <div style={{ marginTop: '20px' }}>
         <Panel>
@@ -31,7 +126,9 @@ const ManageAccount = createReactClass({
               <h5 style={{ fontWeight: 'bold', marginBottom: '0px' }}>
                 Account Type
                 <small>
-                  <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  <TooltipWrapper message={'This is info message'}>
+                    <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  </TooltipWrapper>
                 </small>
               </h5>
               <p style={{ color: 'grey', marginTop: '0px' }}>
@@ -39,11 +136,17 @@ const ManageAccount = createReactClass({
               </p>
               <DropdownButton
                 bsSize={''}
-                title={"Choose an account type"}
+                title={this.state.accountType || "Choose an account type"}
               >
                 {this.state.accountTypes.map(type => {
                   return (
-                    <MenuItem eventKey={type} key={type}>
+                    <MenuItem
+                      active={this.state.accountType === type}
+                      onClick={() => {
+                        this.handleClickAccountType(type)
+                      }}
+                      eventKey={type}
+                      key={type}>
                       {type}
                     </MenuItem>
                   );
@@ -55,7 +158,9 @@ const ManageAccount = createReactClass({
               <h5 style={{ fontWeight: 'bold', marginBottom: '0px' }}>
                 Account name
                 <small>
-                  <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  <TooltipWrapper message={'This is info message'}>
+                    <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  </TooltipWrapper>
                 </small>
               </h5>
               <p style={{ color: 'grey', marginTop: '0px' }}>
@@ -63,11 +168,17 @@ const ManageAccount = createReactClass({
               </p>
               <DropdownButton
                 bsSize={''}
-                title={this.state.accountNames[0] || ''}
+                title={this.state.accountName || 'Choose an account name'}
               >
                 {this.state.accountNames.map(name => {
                   return (
-                    <MenuItem eventKey={name} key={name}>
+                    <MenuItem
+                      onClick={() => {
+                        this.handleClickAccountName(name)
+                      }}
+                      active={this.state.accountName === name}
+                      eventKey={name}
+                      key={name}>
                       {name}
                     </MenuItem>
                   );
@@ -75,28 +186,65 @@ const ManageAccount = createReactClass({
               </DropdownButton>
             </div>
 
+            {this.state.accountType === 'basic_auth_user' && (
+              <div>
+                <h5 style={{ fontWeight: 'bold', marginBottom: '0px' }}>
+                  Odin material setinfo
+                  <small>
+                    <TooltipWrapper message={'This is info message'}>
+                      <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                    </TooltipWrapper>
+                  </small>
+                </h5>
+                <p style={{ color: 'grey', marginTop: '0px' }}>
+                  We use the Odin material set to validate your account credentials at runtime.
+                </p>
+                <FormControl
+                  value={this.state.odinMaterialSet}
+                  onChange={this.handleOdinMaterialSetChange}
+                  type="text"
+                />
+              </div>
+            )}
+
             <div>
               <h5 style={{ fontWeight: 'bold', marginBottom: '0px' }}>
-                Odin material setinfo
+                Region
                 <small>
-                  <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  <TooltipWrapper message={'This is info message'}>
+                    <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  </TooltipWrapper>
                 </small>
               </h5>
               <p style={{ color: 'grey', marginTop: '0px' }}>
-                We use the Odin material set to validate your account credentials at runtime.
+                this will be the region where you use this account in
               </p>
-              <FormControl
-                value={this.state.odinMaterialSet}
-                onChange={this.handleOdinMaterialSetChange}
-                type="text"
-              />
+              <DropdownButton
+                bsSize={''}
+                title={this.state.region || 'Choose region'}
+              >
+                {this.state.regions.map(region => {
+                  return (
+                    <MenuItem
+                      active={this.state.region === region}
+                      onClick={() => {
+                        this.handleClickRegion(region);
+                      }}
+                      eventKey={region} key={region}>
+                      {region}
+                    </MenuItem>
+                  );
+                })}
+              </DropdownButton>
             </div>
 
             <div>
               <h5 style={{ fontWeight: 'bold', marginBottom: '0px' }}>
                 Posix group
                 <small>
-                  <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  <TooltipWrapper message={'This is info message'}>
+                    <Button href={'http://xx.com'} bsStyle={'link'}>Info</Button>
+                  </TooltipWrapper>
                 </small>
               </h5>
               <p style={{ color: 'grey', marginTop: '0px' }}>
@@ -104,16 +252,66 @@ const ManageAccount = createReactClass({
               </p>
               <DropdownButton
                 bsSize={''}
-                title={'Choose and posix group'}
+                title={this.state.posixGroup || 'Choose and posix group'}
               >
                 {this.state.posixGroups.map(group => {
                   return (
-                    <MenuItem eventKey={group} key={group}>
+                    <MenuItem
+                      active={this.state.posixGroup === group}
+                      onClick={() => {
+                        this.handleClickPosixGroup(group);
+                      }}
+                      eventKey={group} key={group}>
                       {group}
                     </MenuItem>
                   );
                 })}
               </DropdownButton>
+
+
+              <div>
+                <DropdownButton bsSize={''} title="选择一个类型" id="dropdown-basic-button">
+                  <div style={{ maxHeight: '200px', overflowY: 'auto', width: '100%', padding: 0 }}>
+                    {options.map(function (option) {
+                      var isSelected = this.state.selected.includes(option.value);
+                      var isHovered = this.state.hover === option.value;
+                      var backgroundColor = isSelected || isHovered ? 'blue' : 'transparent';
+                      var color = isSelected || isHovered ? 'white' : 'black';
+                      return (
+                        <div key={option.value}
+                          onClick={function (event) { this.handleSelect(option.value, event); }.bind(this)}
+                          onMouseEnter={this.handleMouseEnter.bind(this, option.value)}
+                          onMouseLeave={this.handleMouseLeave}
+                          style={{ padding: '10px 20px', cursor: 'pointer', backgroundColor, color }}>
+                          <input
+                            type="checkbox"
+                            checked={isSelected}
+                            onChange={function () { }}
+                            style={{ marginRight: '10px' }}
+                          />
+                          {option.label}
+                        </div>
+                      );
+                    }, this)}
+                  </div>
+                </DropdownButton>
+                <div class="abcde" style={{ display: 'flex', flexWrap: 'wrap', marginTop: '10px' }}>
+                  {this.state.selected.map(function (value) {
+                    var option = options.find(function (option) { return option.value === value; });
+                    return (
+                      <div key={value} style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '5px', marginRight: '5px' }}>
+                        {option.label}
+                        <button onClick={this.handleRemove.bind(this, value)} style={{ marginLeft: '5px' }}>删除</button>
+                      </div>
+                    );
+                  }, this)}
+                </div>
+              </div>
+            </div>
+            <div style={{ marginTop: '30px' }}>
+              <Button
+                onClick={this.handleSubmit}
+                bsStyle="success">Submit</Button>
             </div>
           </Panel.Body>
         </Panel>
