ef2dd70cce57fa45d8d659733ce364f4d8c06167
WanghanlinStephen <47101411+WanghanlinStephen@users.noreply.github.com>
2023-07-24 17:35:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
 var React = require('react');
 var createReactClass = require('create-react-class');
-var Button = require('react-bootstrap').Button;
-var ListGroup = require('react-bootstrap').ListGroup;
-var ListGroupItem = require('react-bootstrap').ListGroupItem;
+var { Grid, Row, Col, Button, Jumbotron } = require('react-bootstrap');
 
 var Dashboard = createReactClass({
-    render: function() {
-        return (
-            <div>
-                <h1>Dashboard Name</h1>
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h3 className="panel-title">
-                            Security Recommendation
-                            <Button bsStyle="default" className="pull-right">Refresh</Button>
-                        </h3>
-                    </div>
-                    <div className="panel-body">
-                        Important metrics go here...
-                    </div>
-                </div>
-                <hr/>
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h3 className="panel-title">
-                            IAM Resources
-                            <Button bsStyle="default" className="pull-right">Refresh</Button>
-                        </h3>
-                    </div>
-                    <div className="panel-body">
-                        <ListGroup>
-                            <ListGroupItem>User Groups: 10</ListGroupItem>
-                            <ListGroupItem>Users: 20</ListGroupItem>
-                            <ListGroupItem>Roles: 30</ListGroupItem>
-                            <ListGroupItem>Policies: 40</ListGroupItem>
-                        </ListGroup>
-                    </div>
-                </div>
-                <hr/>
-                <h2>What's News</h2>
-                <ListGroup>
-                    <ListGroupItem href="#link1">Link 1</ListGroupItem>
-                    <ListGroupItem href="#link2">Link 2</ListGroupItem>
-                    <ListGroupItem href="#link3">Link 3</ListGroupItem>
-                </ListGroup>
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}>
+            <h1>AWS Services</h1>
+            <input type="text" placeholder="Search services, features, marketplace products, and AWS documentation" />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <h2>Identity and Access Management Page</h2>
+            <h3>IAM Dashboard</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <h4>Security Recommendations</h4>
+            <p>Here are some security recommendations...</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <h4>AWS Account</h4>
+            <h5>Access Management</h5>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3}>
+            <h4>User Groups</h4>
+            <Button>Manage User Groups</Button>
+          </Col>
+          <Col md={3}>
+            <h4>Users</h4>
+            <Button>Manage Users</Button>
+          </Col>
+          <Col md={3}>
+            <h4>Roles</h4>
+            <Button>Manage Roles</Button>
+          </Col>
+          <Col md={3}>
+            <h4>Policies</h4>
+            <Button>Manage Policies</Button>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 });
 
 module.exports = Dashboard;
